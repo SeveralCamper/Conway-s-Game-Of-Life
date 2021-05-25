8e1afb22d4e726334f878147eea99a556a24bbb4
zaeskova.vera <zaeskova.vera@bk.ru>
2021-05-26 00:07:15 +0700

    first menu

--- src/gameOfLife/main.cpp
+++ src/gameOfLife/main.cpp
@@ -1,27 +1,35 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace sf;
 
-// #include <SFML/Audio.hpp> - через SFML
-// можно работать с аудио-функциями с помощью
-// подключения данной библиотеки
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+void menu(RenderWindow & window) {
+	Texture menuTexture1, menuTexture2, menuTexture3, aboutTexture, menuBackground;
+	menuTexture1.loadFromFile("image/111.png");
+	menuTexture2.loadFromFile("image/222.png");
+	menuTexture3.loadFromFile("image/333.png");
+	Sprite menu1(menuTexture1), menu2(menuTexture2), menu3(menuTexture3), about(aboutTexture), menuBg(menuBackground);
+	bool isMenu = 1;
+	int menuNum = 0;
+	menu1.setPosition(100, 30);
+	menu2.setPosition(100, 90);
+	menu3.setPosition(100, 150);
+	menuBg.setPosition(345, 0);
+ 
+	//////////////////////////////МЕНЮ///////////////////
+	while (isMenu)
+	{
+		
+	}
+}
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+int main() {
+    RenderWindow window(sf::VideoMode(1376, 768), "LIFE");
+	
+	Event event;
+  	while (window.pollEvent(event))
+  	{
+   	if (event.type == Event::Closed) window.close();
+  	}
 
     return 0;
 }
