01b929ac12fc85a65bbfa1c82b0012db8754a832
zaeskova.vera <zaeskova.vera@bk.ru>
2021-05-27 15:31:18 +0700

    Pictures

--- src/gameOfLife/main.cpp
+++ src/gameOfLife/main.cpp
@@ -3,17 +3,18 @@
 using namespace sf;
 
 void menu(RenderWindow & window) {
-	Texture menuTexture1, menuTexture2, menuTexture3, aboutTexture, menuBackground;
+	Texture menuTexture1, menuTexture2, menuTexture3, menuTexture4,aboutTexture;
 	menuTexture1.loadFromFile("image/111.png");
 	menuTexture2.loadFromFile("image/222.png");
 	menuTexture3.loadFromFile("image/333.png");
-	Sprite menu1(menuTexture1), menu2(menuTexture2), menu3(menuTexture3), about(aboutTexture), menuBg(menuBackground);
+	menuTexture4.loadFromFile("image/444.png");
+	Sprite menu1(menuTexture1), menu2(menuTexture2), menu3(menuTexture3), menu4(menuTexture4), about(aboutTexture);
 	bool isMenu = 1;
 	int menuNum = 0;
-	menu1.setPosition(100, 30);
-	menu2.setPosition(100, 90);
-	menu3.setPosition(100, 150);
-	menuBg.setPosition(345, 0);
+	menu1.setPosition(20, 30);
+	menu2.setPosition(20, 140);
+	menu3.setPosition(20, 240);
+	menu4.setPosition(20, 400);
  
 	//////////////////////////////МЕНЮ///////////////////
 	while (isMenu)
@@ -21,25 +22,31 @@ void menu(RenderWindow & window) {
 		menu1.setColor(Color::White);
 		menu2.setColor(Color::White);
 		menu3.setColor(Color::White);
+		menu4.setColor(Color::White);
+
 		menuNum = 0;
-		window.clear(Color(129, 181, 221));
+		window.clear(Color(255, 255, 255));
  
 		if (IntRect(100, 30, 300, 50).contains(Mouse::getPosition(window))) { menu1.setColor(Color::Blue); menuNum = 1; }
 		if (IntRect(100, 90, 300, 50).contains(Mouse::getPosition(window))) { menu2.setColor(Color::Blue); menuNum = 2; }
 		if (IntRect(100, 150, 300, 50).contains(Mouse::getPosition(window))) { menu3.setColor(Color::Blue); menuNum = 3; }
+		if (IntRect(100, 210, 300, 50).contains(Mouse::getPosition(window))) { menu3.setColor(Color::Blue); menuNum = 4; }
+
  
 		if (Mouse::isButtonPressed(Mouse::Left))
 		{
 			if (menuNum == 1) isMenu = false;//если нажали первую кнопку, то выходим из меню 
-			if (menuNum == 2) { window.draw(about); window.display(); while (!Keyboard::isKeyPressed(Keyboard::Escape)); }
+			if (menuNum == 2) { window.close(); isMenu = false; }
 			if (menuNum == 3)  { window.close(); isMenu = false; }
+			if (menuNum == 4) {  window.close(); isMenu = false; }
  
 		}
  
-		window.draw(menuBg);
 		window.draw(menu1);
 		window.draw(menu2);
 		window.draw(menu3);
+		window.draw(menu4);
+
 		
 		window.display();
 	}
@@ -49,12 +56,19 @@ void menu(RenderWindow & window) {
 int main(int argc, const char** argv) {
     RenderWindow window(sf::VideoMode(1376, 768), "LIFE");
 	
-	Event event;
-  	while (window.pollEvent(event))
-  	{
-   	if (event.type == Event::Closed) window.close();
-  	}
-
+	while (window.isOpen())
+	{
+		Event event;
+  		while (window.pollEvent(event))
+  		{
+   			if (event.type == Event::Closed) window.close();
+  		}
 	menu(window);//вызов меню
+
+	window.display();
+	}
+
+	
+
     return 0;
 }
