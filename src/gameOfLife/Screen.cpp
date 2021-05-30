ca8676f824926f54ab2ddff32a3ee926f005c55f
SeveralCamper <mikha.alkhimovich@mail.ru>
2021-05-31 00:58:46 +0700

    End making zones

--- src/gameOfLife/Screen.cpp
+++ src/gameOfLife/Screen.cpp
@@ -5,6 +5,40 @@ void CloseWindow(sf::RenderWindow& window)
     window.close();
 }
 
+// Создание зоны меню
+void CreateMenuZone(sf::RenderWindow& window)
+{
+    sf::RectangleShape menuZone;
+    menuZone.setSize(sf::Vector2f(
+            (window.getSize().x / 3) - 15, window.getSize().y - 10));
+    menuZone.setPosition(sf::Vector2f(5, 5));
+    window.draw(menuZone);
+}
+
+// Создание игровой зоны
+void CreateGameZone(sf::RenderWindow& window)
+{
+    sf::RectangleShape gameZone;
+    gameZone.setSize(sf::Vector2f(
+            ((window.getSize().x / 3) * 2) - 20, window.getSize().y - 105));
+
+    gameZone.setPosition(sf::Vector2f((window.getSize().x / 3) + 15, 5));
+    window.draw(gameZone);
+}
+
+// Создание зоны отображения статуса игры
+void CreateStatusZone(sf::RenderWindow& window)
+{
+    sf::RectangleShape statusZone;
+    statusZone.setSize(sf::Vector2f(
+        ((window.getSize().x / 3) * 2) - 20, 70));
+
+    statusZone.setPosition(sf::Vector2f(
+            (window.getSize().x / 3) + 15, window.getSize().y - 75));
+
+    window.draw(statusZone);
+}
+
 void Screen(int width, int height, std::string name)
 {
     sf::RenderWindow window(sf::VideoMode(width, height), name);
@@ -27,6 +61,10 @@ void Screen(int width, int height, std::string name)
 
         // Установка цвета фона
         window.clear(sf::Color(0, 220, 100, 0));
+
+        CreateMenuZone(window);
+        CreateGameZone(window);
+        CreateStatusZone(window);
 
         btn.DrawButton();
         btn.IsClickButton(sf::Mouse::getPosition(window));
