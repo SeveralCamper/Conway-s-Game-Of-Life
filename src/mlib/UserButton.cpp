cd5be90f00f967856b3453c171f0335f756852d1
zaeskova.vera <zaeskova.vera@bk.ru>
2021-06-07 18:32:41 +0700

    format

--- src/mlib/UserButton.cpp
+++ src/mlib/UserButton.cpp
@@ -103,7 +103,7 @@ void UserButton::DrawButton()
 // Клик мышью в зоне кнопки
 void UserButton::IsClickButton(sf::Vector2i mousePosition)
 {
-  //  std::cout << "Button" << std::endl;
+    //  std::cout << "Button" << std::endl;
     if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
         if ((mousePosition.x > position.x
              && mousePosition.x < position.x + size.x)
